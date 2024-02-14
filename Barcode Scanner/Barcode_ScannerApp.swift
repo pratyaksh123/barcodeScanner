d11f143d9c41e8171efa09ec7cc2228ed115275b
Pratyaksh <tyagipratyaksh@gmail.com>
2024-02-14 18:27:48 -0500

    Initial Commit

---
+++ Barcode Scanner/Barcode_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barcode_ScannerApp.swift
+//  Barcode Scanner
+//
+//  Created by Pratyaksh Tyagi on 2/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Barcode_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
